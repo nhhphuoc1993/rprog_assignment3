4023c7405537ec75149a1f191ae93f46bbdedd15
Nguyen Ho Hai Phuoc <nhhphuoc1993@gmail.com>
2021-01-06 22:17:06 +0700

    done rankall function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rprog_assignment3
+Assignment: https://www.coursera.org/learn/r-programming/exam/8JIT0/programming-assignment-3-quiz?redirectToCover=true
--- rankall.R
+++ rankall.R
@@ -1,5 +1,5 @@
 require(data.table)
-rankall <- function(outcome, num = "best") {)
+rankall <- function(input_outcome, input_num = "best") {
   
   if(input_outcome == 'heart attack') selected_cols <- c('Hospital Name', 'State', 'Hospital 30-Day Death (Mortality) Rates from Heart Attack')
   else if (input_outcome == 'heart failure') selected_cols <- c('Hospital Name', 'State', 'Hospital 30-Day Death (Mortality) Rates from Heart Failure')
@@ -9,24 +9,14 @@ rankall <- function(outcome, num = "best") {)
   data <- fread("outcome-of-care-measures.csv", select = selected_cols)
   setnames(data, selected_cols, c('hospital_name', 'state', 'rate'))
   
-  if(input_num == 'best') {
-    numeric_data <- data[!is.na(as.numeric(col_name))]
-    
-    
-    order_numeric_data <- numeric_data[order(numeric_data$State, numeric_data$`Hospital Name`)]
-    return(numeric_data[numeric_data[, .I[as.numeric(`Hospital 30-Day Death (Mortality) Rates from Heart Failure`) == min(as.numeric(`Hospital 30-Day Death (Mortality) Rates from Heart Failure`))], by=State]$V1])
-  } else if(input_num == 'worst') {
-    numeric_data <- data[!is.na(as.numeric(data$`Hospital 30-Day Death (Mortality) Rates from Heart Failure`))]
-    order_numeric_data <- numeric_data[order(numeric_data$State, numeric_data$`Hospital Name`)]
-    return(numeric_data[numeric_data[, .I[as.numeric(`Hospital 30-Day Death (Mortality) Rates from Heart Failure`) == max(as.numeric(`Hospital 30-Day Death (Mortality) Rates from Heart Failure`))], by=State]$V1])
-  }
-  else if(is.numeric(input_num) & input_num != 0) {
-    return(specificRank(input_state, input_outcome, input_num))
-  } else stop('Invalid input num!')
+  data <- data[, rate:= as.numeric(rate)]
+  data <- data[!is.na(as.numeric(rate))]
+  data <- data[order(state,rate,hospital_name)]
   
-  ## Check that state and outcome are valid
-  ## For each state, find the hospital of the given rank
-  ## Return a data frame with the hospital names and the
-  ## (abbreviated) state name
-  # numeric_data <- data[!is.na(as.numeric(data$`Hospital 30-Day Death (Mortality) Rates from Heart Failure`))]
+  if(input_num == 'best') return(data[, .SD[1], by = state])#return(data[data[, .I[rate == min(rate)], by=state]$V1])
+  else if(input_num == 'worst') return(data[, .SD[.N], by = state])#return(data[data[, .I[rate == max(rate)], by=state]$V1])
+  else if(is.numeric(input_num) & input_num != 0) return(data[, .SD[input_num], by = state])
+  else stop('Invalid input num!')
 }
+
+# source('rankall.R')
